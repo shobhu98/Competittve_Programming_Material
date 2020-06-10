13c6e98ed3d9ee54cfb58e909929e8e6b8e87da2
SHOBHIT TIWARI <46121289+shobhu98@users.noreply.github.com>
2020-06-10 09:49:49 +0530

    tuple

--- src/Day8/Operation_on_a_tuple.java
+++ src/Day8/Operation_on_a_tuple.java
@@ -10,27 +10,114 @@ public class Operation_on_a_tuple {
         int t=s.nextInt();
 
         for (int i = 0; i <t; i++) {
-            int p=s.nextInt();
-            int q=s.nextInt();
-            int r=s.nextInt();
+            long p=s.nextInt();
+            long q=s.nextInt();
+            long r=s.nextInt();
 
-            int a=s.nextInt();
-            int b=s.nextInt();
-            int c=s.nextInt();
+            long a=s.nextInt();
+            long b=s.nextInt();
+            long c=s.nextInt();
 
-            int d1=a-p;
-            int d2=b-q;
-            int d3=c-r;
-            int step=0;
-           int x=d1+d2+d3;
-            if(x%3==0){
-                System.out.println(x/3-1);
-            }
-            else {
-                System.out.println(x/3);
-            }
+//            long d1=a-p;
+//            long d2=b-q;
+//            long d3=c-r;
+            int flag=0;
+            int step=1;
+         while (true){
+             long d1=a-p;
+             long d2=b-q;
+             long d3=c-r;
+             if(d1==0&&d2==0&d3==0){
+                 System.out.println(0);
+                 break;
+             }
+             if(a/p==b/q&&b/q==c/r&&a%p==0&&b%q==0&&c%r==0){
+                 System.out.println(step);
+                 break;
+             }
+             else if (a%p==0&&b%q==0&&c%r==0) {
+                 System.out.println(step);
+                 break;
+
+
+             }
+
+           else   if((a/p==b/q&&a%p==0&&b%q==0&&c%r!=0)||(c/r==b/q&&c%r==0&&b%q==0&&a%p!=0)||(c/r==a/p&&a%p==0&&c%r==0&&b%q!=0)){
+                 System.out.println(2);
+                 break;
+             }
+             else if((d1==0&&d2!=0&&d3!=0)||(d2==0&&d1!=0&&d3!=0)||(d3==0&&d2!=0&&d1!=0)){
+                 if(d1==0){
+                     if(d2==d3){
+                         System.out.println(1);
+//                         System.out.println("a");
+                         break;
+                     }
+                     else  {
+                         System.out.println(2);
+//                         System.out.println("b");
+
+                         break;
+                     }
+                 }
+                 if(d2==0){
+                     if(d1==d3){
+
+                         System.out.println(1);
+//                         System.out.println("c");
+
+                         break;
+                     }
+                     else  {
+                         System.out.println(2);
+                         break;
+                     }
+                 }
+                 if(d3==0){
+                     if(d1==d2){
+                         System.out.println(1);
+                         break;
+                     }
+                  else    {
+                         System.out.println(2);
+                         break;
+//                         System.out.println("d");
+
+                     }
+                 }
+
+
+
+
+             }
+             else if((d1==0&&d2==0&&d3!=0)||(d2==0&&d1==0&&d3!=0)||(d3==0&&d2==0&&d1!=0)) {
+                 System.out.println(1);
+                 break;
+             }
+             else if((d1==d2&&d2!=d3)||(d2==d3&&d2!=d3)||(d3==d1&&d1!=d3)){
+                 System.out.println(2);
+                 break;
+             }
+             else if(d1==d2&&d2==d3){
+                 System.out.println(1);
+                 break;
+             }
+             else if(d1!=d2&&d2!=d3) {
+                 long d=Math.min(d1,d2);
+                 d=Math.min(d,d3);
+                 p=p+d;
+                 q=q+d;
+                 r=r+d;
+                 step++;
+//                 System.out.println(3);
+//                 break;
+             }
+             }
+
+         }
+
 
 
         }
     }
-}
+
